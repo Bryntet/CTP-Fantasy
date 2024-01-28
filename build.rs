8af92728c70eb6e5b19c787eadb9a615b8f4d386
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-01-28 20:02:29 +0100

    check environment variables on build

---
+++ build.rs
@@ -0,0 +1,21 @@
+
+use std::env;
+
+fn main() {
+    // List of required environment variables
+    let required_vars = ["DATABASE_URL", "FLUTTER_PATH"];
+
+    for &var in &required_vars {
+        match env::var(var) {
+            Ok(value) => {
+                println!("{} is set to {}", var, value);
+                // You can use the value here as needed
+            },
+            Err(_) => {
+                println!("cargo:warning=Required environment variable {} is not set.", var);
+            }
+        }
+    }
+
+    // Rest of your build script logic
+}
