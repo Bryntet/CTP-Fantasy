0ae2be187fe6162e64ddc11d29ca5de1dfe063da
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-03-17 19:18:05 +0100

    test fix

--- service/src/dto/mutation.rs
+++ service/src/dto/mutation.rs
@@ -576,7 +576,9 @@ impl CompetitionInfo {
                 user_scores
                     .into_iter()
                     .dedup_by(|a, b| {
-                        a.pdga_num == b.pdga_num && a.fantasy_tournament_id == b.fantasy_tournament_id
+                        a.competition_id == b.competition_id
+                            && a.pdga_num == b.pdga_num
+                            && a.fantasy_tournament_id == b.fantasy_tournament_id
                     })
                     .map(|p| p.into_active_model(self.competition_id as i32)),
             )
