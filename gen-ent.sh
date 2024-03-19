396028128823cc7d0a44fd7946000e40f4d2d505
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-03-19 09:03:53 +0100

    update entity

--- gen-ent.sh
+++ gen-ent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sea-orm-cli migrate fresh && sea-orm-cli generate entity -l --with-serde deserialize --model-extra-attributes "serde(rename_all = \"PascalCase\")" -o /home/brynte/RustroverProjects/CTP-Fantasy/entity/src
+sea-orm-cli migrate fresh && sea-orm-cli generate entity -l -o /home/brynte/RustroverProjects/CTP-Fantasy/entity/src
